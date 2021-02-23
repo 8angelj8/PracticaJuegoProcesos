289a92e70fa25b8285557a072e4554ed14119166
ivandhh <ivandh202@gmail.com>
2021-02-23 18:25:58 +0100

    Clases jugador y partida

--- src/main/java/Jugador.java
+++ src/main/java/Jugador.java
@@ -1,2 +1,22 @@
 public class Jugador {
+
+    private static String nickname;
+    private static Boolean anfitrion;
+
+    public static String getNickname() {
+        return nickname;
+    }
+
+    public static void setNickname(String nickname) {
+        Jugador.nickname = nickname;
+    }
+
+    public static Boolean getAnfitrion() {
+        return anfitrion;
+    }
+
+    public static void setAnfitrion(Boolean anfitrion) {
+        Jugador.anfitrion = anfitrion;
+    }
+
 }
--- src/main/java/Partida.java
+++ src/main/java/Partida.java
@@ -1,2 +1,42 @@
+import java.util.List;
+
 public class Partida {
+
+    private static int idPartida;
+    private static int tipoPartida;
+    private static int numJugadores;
+    private static List<String> listaJugadores;
+
+    public static int getIdPartida() {
+        return idPartida;
+    }
+
+    public static void setIdPartida(int idPartida) {
+        Partida.idPartida = idPartida;
+    }
+
+    public static int getTipoPartida() {
+        return tipoPartida;
+    }
+
+    public static void setTipoPartida(int tipoPartida) {
+        Partida.tipoPartida = tipoPartida;
+    }
+
+    public static int getNumJugadores() {
+        return numJugadores;
+    }
+
+    public static void setNumJugadores(int numJugadores) {
+        Partida.numJugadores = numJugadores;
+    }
+
+    public static List<String> getListaJugadores() {
+        return listaJugadores;
+    }
+
+    public static void setListaJugadores(List<String> listaJugadores) {
+        Partida.listaJugadores = listaJugadores;
+    }
+
 }
